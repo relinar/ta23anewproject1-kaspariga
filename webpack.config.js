8c24b7fdc9fe48bd43b3de1f884f722c85f47720
Relina <relinarussak@gmail.com>
2024-11-14 17:45:52 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(import.meta.dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader", 
+          {
+            loader: "sass-loader",
+            options: {
+              sassOptions: {
+                quietDeps: true
+              }
+            }
+          }
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+};

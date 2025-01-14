ea160fed9f2712a752cf36f1967a7023ac597a71
relina <relinarussak@gmail.com>
2025-01-14 11:53:19 +0200

    14.01.25

---
+++ kasparitund/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+import { ref } from 'vue';
+
+
+let message = ref('');
+let items = ref(['Redbull', 'Belief', 'Küpsised', 'Näts']);
+
+function addItem(){
+    if(message.value.trim() !== ''){
+        items.value.push(message.value.trim());
+    }
+    message.value = '';
+}
+
+
+</script>
+
+<template>
+    <div class="container mt-2">
+        <div class="field has-addons">
+            <div class="control">
+                <input class="input" type="text" v-model="message" @keypress.enter="addItem">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="addItem">
+                    Add Item
+                </button>
+            </div>
+        </div>
+        <div class="content">
+            <h3>All Items</h3>
+            <ul>
+                <li v-for="item in items">{{ item }}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style></style>

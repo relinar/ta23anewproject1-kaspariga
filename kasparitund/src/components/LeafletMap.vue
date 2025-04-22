3f969006533ee849c834fd36e38fff0bf4889464
relina <relinarussak@gmail.com>
2025-04-22 13:00:20 +0300

    Learnnode maps lehele lisada kodu

--- kasparitund/src/components/LeafletMap.vue
+++ kasparitund/src/components/LeafletMap.vue
@@ -5,32 +5,61 @@ import 'leaflet/dist/images/marker-icon-2x.png';
 import 'leaflet/dist/images/marker-icon.png';
 import 'leaflet/dist/images/marker-shadow.png';
 import { onMounted, useId, watch } from 'vue';
- 
-const {center, zoom} = defineProps(['center', 'zoom']);
+
+const { center, zoom } = defineProps(['center', 'zoom']);
 const id = 'map-' + useId();
 let map;
- 
+
 onMounted(() => {
     map = L.map(id).setView(center, zoom);
+
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    var marker = L.marker([59.42708, 24.74337]).addTo(map);
+
+    const homeLatLng = [59.44297, 24.81264];
+    const blueIcon = new L.Icon({
+        iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x-blue.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
+    const homeMarker = L.marker(homeLatLng, { icon: blueIcon }).addTo(map);
+    homeMarker.bindPopup("Kodu").openPopup();
+
+    const polygon = L.polygon([
+        [homeLatLng[0] + 0.001, homeLatLng[1] - 0.001],
+        [homeLatLng[0] + 0.001, homeLatLng[1] + 0.001],
+        [homeLatLng[0] - 0.001, homeLatLng[1] + 0.001],
+        [homeLatLng[0] - 0.001, homeLatLng[1] - 0.001]
+    ], {
+        color: 'blue',
+        fillColor: '#3399ff',
+        fillOpacity: 0.3
+    }).addTo(map);
 });
-watch(() => center, (center, oldCenter) => {
-    console.log(center, oldCenter);
-    map.panTo(center);
+
+watch(() => center, (newCenter) => {
+    if (map) {
+        map.panTo(newCenter);
+    }
 });
-watch(() => zoom, (zoom) => {
-    map.setZoom(zoom);
-})
+
+watch(() => zoom, (newZoom) => {
+    if (map) {
+        map.setZoom(newZoom);
+    }
+});
 </script>
- 
+
 <template>
     <div :id="id"></div>
 </template>
- 
+
 <style scoped>
 div {
     height: 90vh;
--- kasparitund/src/pages/Leaflet.vue
+++ kasparitund/src/pages/Leaflet.vue
@@ -1,14 +1,14 @@
 <script setup>
 import { ref } from 'vue';
 import LeafletMap from '../components/LeafletMap.vue';
- 
-let center = ref([59.42708, 24.74337]);
+
+let center = ref([59.42708, 24.74337]); 
 let zoom = ref(19);
 </script>
- 
+
 <template>
-    <button class="button is-primary" @click="center=[-25.06623, -130.09967]">Go To Home</button>
+    <button class="button is-primary" @click="center=[59.44297, 24.81264]">Go To Home</button>
     <input type="range" min="1" max="19" v-model="zoom">
     <LeafletMap :center="center" :zoom="zoom"></LeafletMap>
-    <LeafletMap :center="[24.74337, 59.42708]" :zoom="19"></LeafletMap>
+    <LeafletMap :center="[59.44297, 24.81264]" :zoom="19"></LeafletMap>
 </template>
